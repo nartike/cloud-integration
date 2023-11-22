87186952be8d923e1f6d57d004ba203c9191567b
Saathik <77830488+Saathik@users.noreply.github.com>
2023-11-22 12:30:08 +0100

    add service and model

--- src/main/java/fr/efrei/BackendApplication.java
+++ src/main/java/fr/efrei/BackendApplication.java
@@ -1,4 +1,4 @@
-package main.java.fr.efrei;
+package fr.efrei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/fr/efrei/service/ItemService.java
@@ -0,0 +1,4 @@
+package fr.efrei.service;
+
+public class ItemService {
+}
